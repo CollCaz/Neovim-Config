02b11c909a0db56d31c0269bdb1f529abdfb5fb6
CollCaz <yassentombok@proton.me>
2025-02-09 10:17:02 +0200

    typescript ts_ls

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -23,22 +23,7 @@ return {
         gopls = {},
         dartls = {},
         basedpyright = {},
-        tsserver = {
-          init_options = {
-            plugins = {
-              {
-                name = "@vue/typescript-plugin",
-                location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
-                languages = { "javascript", "typescript", "vue" },
-              },
-            },
-          },
-          filetypes = {
-            "javascript",
-            "typescript",
-            "vue",
-          },
-        },
+        ts_ls = {},
         volar = {
           init_options = {
             --filetypes = { "vue", "typescript", "javascript" },
